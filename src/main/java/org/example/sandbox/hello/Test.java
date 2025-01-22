725d8b2f38be4c238186bb2592886c2445459f84
tho2173782 <tho2173782@maricopa.edu>
2025-01-21 18:25:18 -0700

    Update Test.java

--- src/main/java/org/example/sandbox/hello/Test.java
+++ src/main/java/org/example/sandbox/hello/Test.java
@@ -1,5 +1,7 @@
 package org.example.sandbox.hello;
 
-public class Test {
-  System.out.println("Hello, world!");
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 }
