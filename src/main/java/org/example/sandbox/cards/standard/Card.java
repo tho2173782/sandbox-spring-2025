711a1cbc28c5677de261908ce90509060519727e
tho2173782 <tho2173782@maricopa.edu>
2025-02-07 04:21:10 -0700

    Card Deck

--- src/main/java/org/example/sandbox/cards/standard/Card.java
+++ src/main/java/org/example/sandbox/cards/standard/Card.java
@@ -1,113 +1,36 @@
-package org.example.sandbox.cards.standard;
+package org.example.sandbox.Cards.Standard;
 
-/**
- * Represents a single Card
- * We make it Comparable so it's easier to sort.
- * It's also immutable - we don't want the values to change for any instance of the card.
- */
-public class Card
-        implements Comparable<Card> {
+public class Card {
 
     private Suit suit;
-    private FaceValue value;
+    private FaceValue rank;
 
-    private Card() {
-    }
-
-    public Card(Suit suit, FaceValue value) {
+    public Card(Suit suit, FaceValue rank) {
         this.suit = suit;
-        this.value = value;
+        this.rank = rank;
     }
 
     public Suit getSuit() {
-        return this.suit;
+        return suit;
     }
 
-    public FaceValue getValue() {
-        return this.value;
+    public void setSuit(Suit suit) {
+        this.suit = suit;
     }
 
-    /**
-     * This works because of the way ordinals are numbered in enum types.
-     * Quick and dirty way of doing a compareTo method.
-     *
-     * @param card the card you're comparing to
-     * @return -1, 1 or 0 depending on if it's greater, less than or equal to.
-     */
-    @Override
-    public int compareTo(Card card) {
-
-        if (this.getSuit().ordinal() > card.getSuit().ordinal()) {
-            return 1;
-        } else if (this.getSuit().ordinal() < card.getSuit().ordinal()) {
-            return -1;
-        } else {
-
-            if (this.suit == Suit.HEARTS || this.suit == Suit.CLUBS) {
-
-                // ascending sort
-                if (this.getValue().ordinal() > card.getValue().ordinal()) {
-                    return 1;
-                } else if (this.getValue().ordinal() < card.getValue().ordinal()) {
-                    return -1;
-                } else {
-                    return 0;
-                }
-
-            } else {
-
-                // descending sort
-                if (this.getValue().ordinal() < card.getValue().ordinal()) {
-                    return 1;
-                } else if (this.getValue().ordinal() > card.getValue().ordinal()) {
-                    return -1;
-                } else {
-                    return 0;
-                }
-            }
-
-        }
+    public FaceValue getRank() {
+        return rank;
     }
 
-    /**
-     * Implemented only because it's a good idea to do so when implementing equals (used with hash
-     * type collections, HashMap, etc...)
-     *
-     * @return
-     */
-    @Override
-    public int hashCode() {
-        int result = suit != null ? suit.hashCode() : 0;
-        result = 31 * result + (value != null ? value.hashCode() : 0);
-        return result;
-    }
-
-    /**
-     * Used for some collections operations.
-     *
-     * @param o
-     * @return
-     */
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
-
-        Card card = (Card) o;
-
-        if (suit != card.suit)
-            return false;
-        return value == card.value;
+    public void setRank(FaceValue rank) {
+        this.rank = rank;
     }
 
     @Override
     public String toString() {
-
-        final StringBuffer sb = new StringBuffer("Card{");
-        sb.append("value=").append(value);
-        sb.append(", suit=").append(suit);
+        final StringBuilder sb = new StringBuilder("Card{");
+        sb.append("suit=").append(suit);
+        sb.append(", rank=").append(rank);
         sb.append('}');
         return sb.toString();
     }
